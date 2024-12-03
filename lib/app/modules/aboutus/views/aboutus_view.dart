4829a0fd0d5750744f2c044faefb12f3bc93bef3
JonathanZefanya <jonathan.zefanya16@gmail.com>
2024-12-03 23:34:52 +0700

    feat : add profile info widget and theme configuration, include vs_scrollbar dependency

--- lib/app/modules/aboutus/views/aboutus_view.dart
+++ lib/app/modules/aboutus/views/aboutus_view.dart
@@ -37,124 +37,120 @@ class AboutusView extends GetView<AboutusController> {
                 children: [
                   // Logo
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Logo 1
-                      CircleAvatar(
-                        backgroundColor: Colors.white10,
-                        radius: 30,
-                        backgroundImage: AssetImage(
-                            'assets/images/logo.png'), // Ganti dengan path logo 1
-                      ),
-                      SizedBox(width: 16),
-                      // Logo 2
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage(
-                            'assets/images/learningX.jpg'), // Ganti dengan path logo 2
-                      ),
-                      SizedBox(width: 16),
-                      // Logo 3
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage(
-                            'assets/images/msib.jpg'), // Ganti dengan path logo 3
-                      ),
-                    ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Logo 1
+                    CircleAvatar(
+                    backgroundColor: Colors.white10,
+                    radius: 30,
+                    backgroundImage: AssetImage(
+                      'assets/images/logo.png'), // Ganti dengan path logo 1
+                    ),
+                    SizedBox(width: 16),
+                    // Logo 2
+                    CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage(
+                      'assets/images/learningX.jpg'), // Ganti dengan path logo 2
+                    ),
+                    SizedBox(width: 16),
+                    // Logo 3
+                    CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage(
+                      'assets/images/msib.jpg'), // Ganti dengan path logo 3
+                    ),
+                  ],
                   ),
                   SizedBox(height: 16),
                   // Judul
                   Text(
-                    "Ama Laundri",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  "Ama Laundri",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                   ),
                   SizedBox(height: 8),
                   // Deskripsi
                   Text(
-                    "Aplikasi ini dirancang untuk membantu UMKM laundry mengelola operasional mereka dengan mudah.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                  "Aplikasi ini dirancang untuk membantu UMKM laundry mengelola operasional mereka dengan mudah.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, color: Colors.grey[700]),
                   ),
                   SizedBox(height: 24),
                   // Section Misi/Visi
                   ListTile(
-                    leading: Icon(Icons.lightbulb_outline,
-                        color: Colors.orangeAccent),
-                    title: Text(
-                      'Misi Kami',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                        "Memberikan solusi teknologi yang inovatif untuk kebutuhan sehari-hari."),
+                  leading: Icon(Icons.lightbulb_outline,
+                    color: Colors.orangeAccent),
+                  title: Text(
+                    'Misi Kami',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    "Memberikan solusi teknologi yang inovatif untuk kebutuhan sehari-hari."),
                   ),
                   Divider(),
                   // Section Kontak
                   ListTile(
-                    leading:
-                        Icon(Icons.contact_mail, color: Colors.orangeAccent),
-                    title: Text(
-                      'Hubungi Kami',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text("Email: support@ama.com"),
+                  leading:
+                    Icon(Icons.contact_mail, color: Colors.orangeAccent),
+                  title: Text(
+                    'Hubungi Kami',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text("Email: support@ama.com"),
                   ),
                   Divider(),
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Text(
-                      "Tim Kami",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    "Tim Kami",
+                    style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                     ),
                   ),
+                  ),
                   Column(
-                    children: [
-                      // Anggota Tim 1
-                      ListTile(
-                        leading: Icon(
-                          Icons.person,
-                          color: AppColors.accent,
-                        ),
-                        title: Text("Jonatahan Natannael Zefany"),
-                        subtitle: Text("Institut Teknologi Indonesia"),
-                      ),
-                      Divider(),
-                      // Anggota Tim 2
-                      ListTile(
-                        leading: Icon(
-                          Icons.person,
-                          color: AppColors.accent,
-                        ),
-                        title: Text("Bryan Hanggara"),
-                        subtitle: Text("Universitas Sriwijaya"),
-                      ),
-                      Divider(),
-                      // Anggota Tim 3
-                      ListTile(
-                        leading: Icon(
-                          Icons.person,
-                          color: AppColors.accent,
-                        ),
-                        title: Text("Rian Satria Permana"),
-                        subtitle: Text(
-                            "Sekolah Tinggi Teknologi Terpadu Nurul Fikri"),
-                      ),
-                      Divider(),
-                      // Anggota Tim 3
-                      ListTile(
-                        leading: Icon(
-                          Icons.person,
-                          color: AppColors.accent,
-                        ),
-                        title: Text("Yohanna Gloria"),
-                        subtitle: Text("Universitas Paramadina"),
-                      ),
-                    ],
+                  children: [
+                    // Anggota Tim 1
+                    ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/avatar/jojo.jpg'),
+                    ),
+                    title: Text("Jonatahan Natannael Zefanya"),
+                    subtitle: Text("Institut Teknologi Indonesia"),
+                    ),
+                    Divider(),
+                    // Anggota Tim 2
+                    ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/avatar/bryan.jpg'),
+                    ),
+                    title: Text("Bryan Hanggara"),
+                    subtitle: Text("Universitas Sriwijaya"),
+                    ),
+                    Divider(),
+                    // Anggota Tim 3
+                    ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/avatar/rian.jpg'),
+                    ),
+                    title: Text("Rian Satria Permana"),
+                    subtitle: Text(
+                      "Sekolah Tinggi Teknologi Terpadu Nurul Fikri"),
+                    ),
+                    Divider(),
+                    // Anggota Tim 4
+                    ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/avatar/yohanna.jpg'),
+                    ),
+                    title: Text("Yohanna Gloria"),
+                    subtitle: Text("Universitas Paramadina"),
+                    ),
+                  ],
                   ),
                 ],
               ),
--- lib/component/app_color.dart
+++ lib/component/app_color.dart
@@ -24,3 +24,21 @@ class Constants {
 
   static var errorColor; // Warna background menu
 }
+
+const LinearGradient primaryGradient = LinearGradient(
+    colors: [Color.fromARGB(255, 109, 230, 90), Color.fromARGB(255, 86, 233, 111)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter);
+
+const Color blueColor = Color(0xFF5680E9);
+const Color yellowColor = Color(0xFFFFE45E);
+const Color blue2Color = Color(0xFF63B1EF);
+const Color blue3Color = Color(0xFFCBD6F3);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color greyColor = Color(0xFF878F95);
+
+const FontWeight weightBold = FontWeight.bold;
+const FontWeight weightNormal = FontWeight.normal;
+const FontWeight weightMedium = FontWeight.w500;
+const FontWeight weightSemiBold = FontWeight.w600;
+const FontWeight weightLight = FontWeight.w300;
